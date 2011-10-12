de03eaf5344ed5ab70703972af165b2b879bad77
Rob Snow <snowystinger@gmail.com>
2011-10-11 17:39:21 -0700

    organization repo

---
+++ src/com/ucla/MotionCaptureActivity.java
@@ -0,0 +1,13 @@
+package com.ucla;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MotionCaptureActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
